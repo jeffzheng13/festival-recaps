4d651968a78e4dfdfd53806bd179707dc21eb224
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2024-12-28 01:08:21 -0500

    made homepage text pop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Box, Container, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-cover bg-center bg-[url('../../public/rave-background.jpg')] flex items-center justify-center">
-      <div className="items-center">
+    <div className="relative h-screen bg-cover bg-center bg-[url('../../public/rave-background.jpg')] flex items-center justify-center">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 items-center">
         <Box>
           <Container>
             <Typography variant="h1" sx={{fontWeight: 500}}>WELCOME TO FESTIVAL RECAPS</Typography>
-            <Typography variant="subtitle1">Learn More →</Typography>
+            <Link href="/raves"><Typography variant="subtitle1" className="hover:underline">Explore More →</Typography></Link>
           </Container>
         </Box>
       </div>
